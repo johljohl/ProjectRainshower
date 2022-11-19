5d1b7a5a688cd1b30d40d18f294f69773309e49f
johljohl <joohan.lindell@gmail.com>
2022-11-19 15:15:51 +0100

    restore page

--- script.js
+++ script.js
@@ -1,46 +1,113 @@
-const mario = document.getElementById("mario");
-const goomba = document.getElementById("goomba");
-const score = document.getElementById("score");
+// aplication Data
 
-let mySound = new Audio("sound/mariojump.mp3");
+const cityLocation = "Helsingborg"; // change this if you want a different city location
+const apiKey = "c7c70ffe100679d44d96ba6069ce2b4d"; // The apiKey for the weatherapplication
+const celsius = "=metric"; // to convert the temperature
 
-let myMusic = new Audio("sound/theme.mp3");
+// DOM elements
 
-function jump() {
-  mario.classList.add("jump");
-  setTimeout(() => {
-    mario.classList.remove("jump");
-  }, 500);
+const weatherLocation = document.querySelector(".location");
+const weatherIcon = document.querySelector(".weatherIcons");
+const weatherTemp = document.querySelector(".weatherTempValue");
+
+// Below is the geoposition function for getting loaction information  with help from AJAX requests
+
+function getGeoPosition() {
+  let urlGeo = `https://api.openweathermap.org/geo/1.0/direct?q=${cityLocation}&limit=5&appid=${apiKey}`;
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", urlGeo);
+  xhr.responseType = "text";
+
+  xhr.send();
+
+  xhr.addEventListener("load", () => {
+    if (xhr.status === 200) {
+      console.log("success");
+      data = JSON.parse(xhr.responseText);
+      console.log(data);
+      console.log(data[0]["lat"]);
+      console.log(data[0]["lon"]);
+      let lat = data[0]["lat"];
+      let long = data[0]["lon"];
+
+      getWeather(lat, long);
+    } else {
+      console.log("error" + xhr.status);
+    }
+  });
 }
-document.addEventListener("click", function (event) {
-  if (!mario.classList.contains("jump")) {
-    jump();
-    mySound.play();
-    myMusic.play();
-  }
-});
 
-setInterval(() => {
-  score.innerText++;
-  const marioTop = parseInt(
-    window.getComputedStyle(mario).getPropertyValue("top")
-  );
-  const goombaLeft = parseInt(
-    window.getComputedStyle(goomba).getPropertyValue("left")
-  );
+// Below is the main function for getting weather information  with help from AJAX requests
 
-  if (goombaLeft < 0) {
-    goomba.style.display = "none";
-  } else {
-    goomba.style.display = "";
-  }
+function getWeather(lat, long) {
+  let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units${celsius}`;
+  console.log(urlWeather);
+  xhr = new XMLHttpRequest();
+  xhr.open("GET", urlWeather);
+  xhr.reponseType = "text";
 
-  if (goombaLeft < 50 && goombaLeft > 0 && marioTop > 150) {
-    alert(
-      "Game Over man, game over!! \n\nYour score was: " +
-        score.innerText +
-        "\n\nPress a key to play again!"
-    );
-    location.reload();
+  xhr.send();
+
+  xhr.addEventListener("load", () => {
+    if (xhr.status === 200) {
+      weatherLocation.textContent = "success";
+      data = JSON.parse(xhr.responseText);
+      console.log(data);
+
+      showWeather();
+    } else {
+      console.log("error" + xhr.status);
+    }
+  });
+}
+
+// showWeather fuction builds the weather data
+
+function showWeather() {
+  // name, country, icons, temp, description,
+  city = data.name;
+  country = data.sys.country;
+  icons = data.weather[0].icon;
+  temp = Math.round(data.main.temp); // uses math.round to rounded to the nearest integer
+  tempDescription = data.weather[0].description;
+
+  weatherLocation.innerHTML = `${city}, ${country}`;
+  weatherTemp.innerHTML = `${temp}°C, ${tempDescription}`;
+  weatherIcon.innerHTML = `<img src="img/32x32/${icons}.png" />`;
+}
+
+//-------------the function below is for the watch----//
+
+function watch() {
+  const today = new Date();
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
+  let day = today.getDate();
+  let month = today.getMonth();
+  month++;
+  minutes = checkTime(minutes);
+  seconds = checkTime(seconds);
+
+  document.getElementById("timer").innerHTML = `Todays date
+    ${day} / ${month} 
+    Time ${hour}:${minutes}:${seconds}`;
+  setTimeout(watch, 1000);
+}
+
+function checkTime(i) {
+  // to add a zero in front of minutes and seconds
+  if (i < 10) {
+    i = "0" + i;
   }
-}, 50);
+  return i;
+}
+
+// The function below is for the init function which loads the page//
+function init() {
+  console.log("connected");
+  getGeoPosition();
+  watch();
+}
+
+init();
