df9839e011c5fa5f91910d1294d3324688782145
johljohl <joohan.lindell@gmail.com>
2022-11-19 13:42:12 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,81 +1,3 @@
-// aplication Data
-
-const cityLocation = "Helsingborg"; // change this if you want a different city location
-const apiKey = "c7c70ffe100679d44d96ba6069ce2b4d"; // The apiKey for the weatherapplication
-const celsius = "=metric"; // to convert the temperature
-
-// DOM elements
-
-const weatherLocation = document.querySelector(".location");
-const weatherIcon = document.querySelector(".weatherIcons");
-const weatherTemp = document.querySelector(".weatherTempValue");
-
-// Below is the geoposition function for getting loaction information  with help from AJAX requests
-
-function getGeoPosition() {
-  let urlGeo = `https://api.openweathermap.org/geo/1.0/direct?q=${cityLocation}&limit=5&appid=${apiKey}`;
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", urlGeo);
-  xhr.responseType = "text";
-
-  xhr.send();
-
-  xhr.addEventListener("load", () => {
-    if (xhr.status === 200) {
-      console.log("success");
-      data = JSON.parse(xhr.responseText);
-      console.log(data);
-      console.log(data[0]["lat"]);
-      console.log(data[0]["lon"]);
-      let lat = data[0]["lat"];
-      let long = data[0]["lon"];
-
-      getWeather(lat, long);
-    } else {
-      console.log("error" + xhr.status);
-    }
-  });
-}
-
-// Below is the main function for getting weather information  with help from AJAX requests
-
-function getWeather(lat, long) {
-  let urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units${celsius}`;
-  console.log(urlWeather);
-  xhr = new XMLHttpRequest();
-  xhr.open("GET", urlWeather);
-  xhr.reponseType = "text";
-
-  xhr.send();
-
-  xhr.addEventListener("load", () => {
-    if (xhr.status === 200) {
-      weatherLocation.textContent = "success";
-      data = JSON.parse(xhr.responseText);
-      console.log(data);
-
-      showWeather();
-    } else {
-      console.log("error" + xhr.status);
-    }
-  });
-}
-
-// showWeather fuction builds the weather data
-
-function showWeather() {
-  // name, country, icons, temp, description,
-  city = data.name;
-  country = data.sys.country;
-  icons = data.weather[0].icon;
-  temp = Math.round(data.main.temp); // uses math.round to rounded to the nearest integer
-  tempDescription = data.weather[0].description;
-
-  weatherLocation.innerHTML = `${city}, ${country}`;
-  weatherTemp.innerHTML = `${temp}°C, ${tempDescription}`;
-  weatherIcon.innerHTML = `<img src="img/32x32/${icons}.png" />`;
-}
-
 //-------------the function below is for the watch----//
 
 function watch() {
@@ -104,10 +26,5 @@ function checkTime(i) {
 }
 
 // The function below is for the init function which loads the page//
-function init() {
-  console.log("connected");
-  getGeoPosition();
-  watch();
-}
 
-init();
+watch();
